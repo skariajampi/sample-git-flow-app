a1eff7b8119f90afdb9b7fdfc0144719a0a3608a
skariaj <skariajampi@gmail.com>
2026-04-07 11:31:43 +0100

    added minimal spring boot code to test

---
+++ src/main/java/com/example/samplegitflowspringboot/SampleGitFlowSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.samplegitflowspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleGitFlowSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleGitFlowSpringBootApplication.class, args);
+    }
+
+}
